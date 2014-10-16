15c5a21be8c1e8912a34dfecbc3dde777d697af7
robert berry <berrydigital@gmail.com.com>
2014-10-16 01:34:50 -0700

    drafting demos

--- javascripts/pgantenna.js
+++ javascripts/pgantenna.js
@@ -4,7 +4,7 @@ $(window).scroll(function (event) {
       opace = Math.min(Math.pow((scroll / 800.0), 4), 1);
 
 			if (opace == 1) {
-			  $(".navbar").css("border-bottom", "1px solid");
+			  $(".navbar").css("border-bottom", "3px solid");
 			} else {
 			  $(".navbar").css("border-bottom", "0px");
 			}
